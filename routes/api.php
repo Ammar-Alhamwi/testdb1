47a72ece2a6a088afd1d3aac068cee52a000be8a
Ammar-Alhamwi <ammaralhamwi98@gmail.com>
2022-03-21 14:14:44 +0200

    done update

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login','AuthController@login');
+
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/order', 'orderController@createOrder');
     Route::put('/table/{id}', 'orderController@update');
